b964b43d5a381e85474a3c522fd8cb431537d497
Tanmay <tanmaysharma763@gmail.com>
2024-12-28 13:59:39 +0530

    error

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -5,6 +5,7 @@ import { useRouter, useSearchParams } from "next/navigation";
 
 import Form from "@components/Form";
 
+
 const UpdatePrompt = () => {
   const router = useRouter();
   const searchParams = useSearchParams();
